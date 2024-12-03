e3ac2a382d510ea8b775c3b91d04bd2cb4b2b4ba
Stradalec <pochtatipo6@gmail.com>
2024-12-03 13:12:43 +0700

    Added coolest toolbar ever

--- app/src/main/java/com/example/labandroid8/MainActivity.kt
+++ app/src/main/java/com/example/labandroid8/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import retrofit2.Call
@@ -31,6 +32,11 @@ class MainActivity : AppCompatActivity() {
         val units = "metric"
 
         val call = service.getForecast(city, units, apiKey)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Шклов"
+
 
         call.enqueue(object : Callback<Forecast> {
             override fun onResponse(call: Call<Forecast>, response: Response<Forecast>) {
