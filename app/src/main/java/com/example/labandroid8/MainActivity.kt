0e1835e8459cff884f4401bbb035be59e988aac8
Stradalec <pochtatipo6@gmail.com>
2024-12-03 12:59:56 +0700

    Added data classes (again)

--- app/src/main/java/com/example/labandroid8/MainActivity.kt
+++ app/src/main/java/com/example/labandroid8/MainActivity.kt
@@ -1,16 +1,59 @@
 package com.example.labandroid8
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
+        val service = retrofit.create(OpenWeatherMapService::class.java)
+
+        val apiKey = "ab1700c1546c934f4cdc73aeab7eb4d3"
+        val city = "Шклов"
+        val units = "metric"
+
+        val call = service.getForecast(city, units, apiKey)
+
+        call.enqueue(object : Callback<Forecast> {
+            override fun onResponse(call: Call<Forecast>, response: Response<Forecast>) {
+                if (response.isSuccessful) {
+                    val forecast = response.body()
+                }
+                else
+                {
+
+                }
+            }
+
+            override fun onFailure(call: Call<Forecast>, t: Throwable) {
+            }
+        })
     }
+}
+
+interface OpenWeatherMapService {
+    @GET("forecast")
+    fun getForecast(
+        @Query("q") city: String,
+        @Query("units") units: String,
+        @Query("appid") apiKey: String
+    ): Call<Forecast>
 }
